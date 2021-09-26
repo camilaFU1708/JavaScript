8c78d39257816cc1c9921e8166d1173fc888037c
camilaFU1708 <44846079+camilaFU1708@users.noreply.github.com>
2021-09-26 19:40:00 -0300

    Commit clase 6

---
+++ Clase 6/JS/clase6.js
@@ -0,0 +1,6 @@
+
+const MisHamburguesas =["Hamburguesa cheddar ", " Hamburguesa clasica ", "Hamburguesa bacon " , "Hamburguesa pollo "  , "Hamburguesa gurmet " ];
+
+console.log (MisHamburguesas);
+
+console.log (MisHamburguesas.join ("25% de descuento en efectivo"));
